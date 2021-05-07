1e7258f3d2fa1deed6b65aa58cdb83bf618eeb9f
Laura <keenlaura@hotmail.co.uk>
2021-05-07 11:31:22 +0100

    Changed hard coded test for viewing all peeps to use database instead

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,5 +1,11 @@
+require 'pg'
+
 feature 'View peeps' do
   scenario 'A user can view peeps' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    connection.exec("INSERT INTO peeps(message) VALUES('My first peep!');")
+
     visit('/peeps')
 
     expect(page).to have_content "My first peep!"
