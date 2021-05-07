f17f3d07fcbdfdcd42d8e2ae76ea6417014bf756
Laura <keenlaura@hotmail.co.uk>
2021-05-07 11:23:35 +0100

    Used pg gem to set up testing environment using a test data base instead of the actual data base

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,7 +3,12 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES ('My first peep!');")
+
       peeps = Peep.all
+      
       expect(peeps).to include("My first peep!")
     end
   end
